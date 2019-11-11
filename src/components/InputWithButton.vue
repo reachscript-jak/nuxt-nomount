332ac83764f4bd68d5ed87ff9dfb7d970648af24
Takumi Okada <t_okada@freelance-jak.com>
2019-11-12 00:09:41 +0900

    シャッフルの基本ロジック作成

---
+++ src/components/InputWithButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-model="name" placeholder="Input name here" />
+    <button @click="handleClick">この人</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputWithButton',
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('regist', this.name)
+    }
+  }
+}
+</script>
---
+++ src/pages/shuffle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="root">
+    <p>シャッフルのページです</p>
+    <button @click="add">追加</button>
+    <div v-for="(speaker, idx) in speakersArr" v-bind:key="idx">
+      <InputWithButton @regist="regist" />
+    </div>
+    <div>登録されたユーザ</div>
+    <div v-for="name in speakersName" v-bind:key="name">{{ name }}</div>
+    <button @click="dispSpeakers">スピーカー表示</button>
+  </div>
+</template>
+
+<script>
+import InputWithButton from '@/components/InputWithButton'
+export default {
+  components: {
+    InputWithButton
+  },
+  data() {
+    return {
+      speakersArr: [],
+      speakersName: []
+    }
+  },
+  methods: {
+    add() {
+      this.speakersArr.push('')
+    },
+    regist(name) {
+      this.speakersName.push(name)
+    },
+    dispSpeakers() {
+      console.log(this.speakersName)
+    }
+  }
+}
+</script>
+
+<style>
+.root {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
